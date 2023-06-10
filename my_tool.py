8bffdce692cf822e3ee2aba2c0cc32f092626773
superegg <1969383133@qq.com>
2023-06-11 05:36:07 +0800

    task2

--- my_tool.py
+++ my_tool.py
@@ -1,13 +1,13 @@
 import cv2
 
-img=cv2.imread("/home/superegg/PycharmProjects/OpenMMLab/MMPose_Tutorials/2023/0524/mmpose/outputs/B1_HRNet_1/multi-person.jpeg")
+img=cv2.imread("/home/superegg/PycharmProjects/OpenMMLab/task1_MMPose/2023/0524/mmpose/outputs/B1_HRNet_1/multi-person.jpeg")
 cv2.namedWindow("img",0)
 cv2.resizeWindow("img",1080,720)
 cv2.imshow('img',img)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-video=cv2.VideoCapture("/home/superegg/PycharmProjects/OpenMMLab/MMPose_Tutorials/2023/0524/mmpose/outputs/B1_RTM_2/cxk.mp4")
+video=cv2.VideoCapture("/home/superegg/PycharmProjects/OpenMMLab/task1_MMPose/2023/0524/mmpose/outputs/B1_RTM_2/cxk.mp4")
 while(video.isOpened()):
     retval,image=video.read()
     # cv2.namedWindow("Video",0)
